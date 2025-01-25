afaecc34c1f647bb9f8b0e28a45d5c77d19bb0f4
Shamveel P <shamveelp007@gmail.com>
2025-01-25 16:04:40 +0530

    model setup

---
+++ models/bannerSchema.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const bannerSchema = new Schema({
+    image:{
+        type: String,
+        required: true
+    },
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    link:{
+        type: String,
+        required: true
+    },
+    startDate:{
+        type: Date,
+        required: true
+    },
+    endDate:{
+        type: Date,
+        required: true
+    },
+})
+
+const Banner = mongoose.model('Banner', bannerSchema);
+
+module.exports = Banner;
---
+++ models/productSchema.js
@@ -0,0 +1,59 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const productSchema = new Schema({
+    productName:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    brand:{
+        type: String,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    },
+    regularPrice:{
+        type: Number,
+        required: true
+    },
+    salePrice:{
+        type: Number,
+        required: true
+    },
+    productOffer:{
+        type: Number,
+        default: 0
+    },
+    quantity:{
+        type: Number,
+        required: true
+    },
+    color:{
+        type: String,
+        required: true
+    },
+    productImage:{
+        type: [String],
+        required: true
+    },
+    isBlocked:{
+        type: Boolean,
+        default: false
+    },
+    status:{
+        type: String,
+        enum:['available','out of stock','Discontinued'],
+        required: true,
+        default: 'available'
+    },
+},{timestamps: true})
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
